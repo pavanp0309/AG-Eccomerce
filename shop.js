d43275f1c7fbffad38e89931c9e493d6aa86ece5
pavanp0309 <pavanreddyp0309@gmail.com>
2024-10-19 11:48:34 +0530

    add

---
+++ shop.js
@@ -0,0 +1,14 @@
+// Loading all the Items (html elements in the js) when browser is loaded
+document.addEventListener("DOMContentLoaded",()=>{
+    let addtoCartBtn=document.querySelectorAll('.')
+})
+
+// adding items to the cart
+
+
+// updating the cartPage
+
+
+// increment the cartIcon value
+
+
